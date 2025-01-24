b1f4b25adda04af0c370be018467897c9a156bb7
Diogo_MVP <98986763+Dimvper@users.noreply.github.com>
2025-01-24 22:14:48 +0000

    Updated configs

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -106,27 +106,71 @@ return {
   { "rose-pine/neovim", name = "rose-pine" },
   { "nyoom-engineering/oxocarbon.nvim" },
   { "sainnhe/gruvbox-material" },
+  { "whatyouhide/vim-gotham" },
+  -- Lua
   {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = false,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
+    "folke/zen-mode.nvim",
     opts = {
-      workspaces = {
-        {
-          name = "work",
-          path = "~/vaults/work",
+      window = {
+        backdrop = 0.95,
+        width = 0.85,
+        height = 1,
+        options = {
+          signcolumn = "no",
+          number = false,
+          relativenumber = false,
+          cursorline = false,
+          cursorcolumn = false,
+          foldcolumn = "0",
         },
       },
+    },
+    config = function(_, opts)
+      require("zen-mode").setup(opts)
+    end,
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      direction = "vertical",
+      size = 55,
+      -- float_opts = {
+      --   border = "curved",
+      -- },
+    },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+
+      function _G.set_terminal_keymaps()
+        local opts_ = { buffer = 0 }
+        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts_)
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts_)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts_)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts_)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts_)
+      end
+
+      vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+    end,
+    cmd = {
+      "ToggleTerm",
+      "ToggleTermToggleAll",
+      "TermExec",
+      "TermSelect",
+      "ToggleTermSetName",
+      "ToggleTermSendVisualLines",
+      "ToggleTermSendVisualSelection",
+      "ToggleTermSendCurrentLine",
+    },
+    keys = {
+      { "<leader>t", "<cmd>ToggleTerm<cr>", desc = "Toggle [T]erminal" },
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "oxocarbon",
+      colorscheme = "gotham",
     },
   },
 }
