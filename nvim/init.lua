6532650f58fc0876034ea8d4735244b2217ebdb1
Diogo_MVP <98986763+Dimvper@users.noreply.github.com>
2025-06-06 09:05:57 +0100

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
--- Detect if running in VSCode
-if vim.g.vscode then
-  -- Load VSCode-specific configuration
-  require("config.vscode")
-else
-  -- Load regular LazyVim configuration for terminal use
-  require("config.lazy")
-end
